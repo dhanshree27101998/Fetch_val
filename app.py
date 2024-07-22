7f450e95b0c21b2b01e8748045c3746662d6166f
dhanshree27101998 <dhanshreechandak98@gmail.com>
2024-07-22 06:42:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ import os
 variable_value = os.getenv('TEST1')
 print(f'The value of TEST1 is {variable_value}')
 
+variable_value1 = os.getenv('USER')
+print(f'The value of USER is {variable_value1}')
