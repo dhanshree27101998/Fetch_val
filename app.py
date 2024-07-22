0983d36ae772070c780c847f157cb68e7917712c
dhanshree27101998 <dhanshreechandak98@gmail.com>
2024-07-22 12:37:23 -0400

    added secrets

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import subprocess
 variable_value = os.getenv('TEST1')
 print(f'The value of TEST1 is {variable_value}')
 
+if os.getenv('TEST1')=="dhanshree":
+    testval=os.getenv('TEST1')
+    print("Val is correct")
+    print(f'testval is {testval}')
+else:
+    print("Val is not correct")    
+
 variable_value1 = os.getenv('USER')
 print(f'The value of USER is {variable_value1}')
 
