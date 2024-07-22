89fae2aa18cf28cd3830b934d626774967328b34
dhanshree27101998 <dhanshreechandak98@gmail.com>
2024-07-22 16:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 # Fetch environment variables
 variable_value = os.getenv('TEST1')
-print(f'The value of TEST1 is {variable_value}')
+print(rf'The value of TEST1 is {variable_value}')
 
 variable_value1 = os.getenv('USER')
 print(f'The value of USER is {variable_value1}')
