f090193b511db08e93181db97bf6efcad9b4c54e
dhanshree27101998 <dhanshreechandak98@gmail.com>
2024-07-23 06:27:52 -0400

    added secrets

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from cryptography.fernet import Fernet
 
 # Fetch environment variables
 variable_value = os.getenv('TEST1')
@@ -18,8 +19,28 @@ print(f'The value of USER is {variable_value1}')
 variable_value = os.getenv('TEST2')
 print(f'The value of TEST2 is {variable_value}')
 
-# Run the printenv command
-result = subprocess.run(['printenv'], capture_output=True, text=True)
+
+# Encryption key (generate and store securely)
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+
+# Access the secret value
+my_secret = os.getenv('TEST1')
+
+# Encrypt the secret value
+encrypted_secret = cipher_suite.encrypt(my_secret.encode())
+
+# Print the encrypted secret value
+print(f"Encrypted secret value: {encrypted_secret}")
+
+# Decrypt the secret value
+decrypted_secret = cipher_suite.decrypt(encrypted_secret).decode()
+
+print(f"Decrypted secret value: {decrypted_secret}")
+
+
+# # Run the printenv command
+# result = subprocess.run(['printenv'], capture_output=True, text=True)
  
-# Print the output
-print(result.stdout)
+# # Print the output
+# print(result.stdout)
